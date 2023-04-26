31ed4bb201851ac7b5ad79f3e54368354527f7a6
Ahmet Oğuz Ergin <ahmettoguz@hotmail.com>
2023-04-26 11:40:18 +0300

    View Binding added.

--- app/src/main/java/com/ctis487/ahmetoguzergin/hw2/LoginActivity.java
+++ app/src/main/java/com/ctis487/ahmetoguzergin/hw2/LoginActivity.java
@@ -2,13 +2,39 @@ package com.ctis487.ahmetoguzergin.hw2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Toast;
+
+import com.ctis487.ahmetoguzergin.hw2.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
+    ActivityLoginBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+
+        // hide title bar
+        getSupportActionBar().hide();
+        setContentView(view);
+
+        // hide status bar
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        // lock orientation
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        binding.LoginBtnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(LoginActivity.this, "Clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
--- app/src/main/java/com/ctis487/ahmetoguzergin/hw2/MainActivity.java
+++ app/src/main/java/com/ctis487/ahmetoguzergin/hw2/MainActivity.java
@@ -1,13 +1,33 @@
+
 package com.ctis487.ahmetoguzergin.hw2;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+
+import com.ctis487.ahmetoguzergin.hw2.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+
+        // hide title bar
+        getSupportActionBar().hide();
+        setContentView(view);
+
+        // hide status bar
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        // lock orientation
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
     }
 }
